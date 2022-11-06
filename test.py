f6a89028411f21a722c48b5f5259002b31f49ae0
vitaliy <vorob2008@yandex.ru>
2022-11-06 15:21:38 +0300

    test yml

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from payment import *
 
- def test (a,b):
-     c = calculation(a,b)
-     test_c = a+b
 
-     assert c == test_c
+def test(a, b):
+    c = calculation(a, b)
+    test_c = a+b
+    assert c == test_c
 
-test(10,20)
-test(1,15)
-test(40,150)
+
+test(10, 20)
+test(1, 15)
+test(40, 150)
